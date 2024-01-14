2d58628fd161e287575ec5ccd2b37acc3da19a26
Amarildo <amarildos@outloo.it>
2024-01-14 12:28:32 +0100

    added function button and crated grid

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 console.log('JS OK');
 
+// Recupero elementi
+const grid = document.getElementById('grid');
+const button = document.getElementById('play-button');
+
+
+
+
+//funzioni
+
+const startGame = () => {
+
+    button.innerText = 'Restart';
+    //Svuota griglia
+    grid.innerText = '';
+
+    //Grid
+    for (let i = 1; i <= 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.append(i);
+        grid.appendChild(cell);
+    }
+};
+
+
+// listener button
+
+button.addEventListener('click', startGame());
